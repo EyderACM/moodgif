0cb0ca9f97f0afcb32660dca5359dc7fce9bf2aa
Eyder A. Concha <eyderacm@gmail.com>
2021-01-06 21:08:49 -0600

    feat: make properly responsive

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,8 +9,12 @@ const Home = () => {
     <div>
       <NightmodeToggle />
       <HomeWrapper>
-        <Flex direction="column" width="80%" maxW="700px">
-          <Stack align="center" spacing={2} padding="10vw">
+        <Flex direction="column" width={{ xl: "80%" }} maxW="700px">
+          <Stack
+            align="center"
+            spacing={2}
+            padding={{ md: "10vw", xl: "20px" }}
+          >
             <Header />
             <GifInput />
           </Stack>
